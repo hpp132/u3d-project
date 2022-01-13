7a10dd15f2c30268ba3d486180a93e41927636fa
pengpeng.huang <pengpeng.huang@ushow.com>
2022-01-13 17:32:04 +0800

    init

---
+++ Assets/playerMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.log(222);
+        // string aa = "sdfsdf";
+        print(1);
+        // print(aa);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
